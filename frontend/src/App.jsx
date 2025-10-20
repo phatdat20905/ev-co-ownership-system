42a2e550871fd77eb31585fef2f5f992c7be3119
NguyenThanhChien-2010 <thanhhchienn2636@gmail.com>
2025-10-21 00:56:32 +0700

    Update 4 route moi(Forgot, Verify, Reset, QuyDinh)

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,25 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import ScrollToTop from "./components/ScrollToTop";
-import Header from "./components/Header";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
-
+import ForgotPassword from "./pages/ForgotPassword";
+import VerifyEmail from "./pages/VerifyEmail";
+import ResetPassword from "./pages/ResetPassword";
+import QuyDinhHoatDong from "./pages/policies/QuyDinhHoatDong";
 
 export default function App() {
   return (
     <BrowserRouter>
       <ScrollToTop />
-      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/forgot-password" element={<ForgotPassword />} />
+        <Route path="/verify-email" element={<VerifyEmail />} />
+        <Route path="/reset-password" element={<ResetPassword />} />
+        <Route path="/quy-dinh-hoat-dong" element={<QuyDinhHoatDong />} />
       </Routes>
     </BrowserRouter>
   );
