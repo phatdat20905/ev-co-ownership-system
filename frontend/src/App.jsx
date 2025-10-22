682c5085b4abf70d8ac2e559c3d3fab802a35be5
NguyenThanhChien-2010 <thanhhchienn2636@gmail.com>
2025-10-22 15:14:43 +0700

    Update them cac Route cho UI

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,26 +1,38 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import ScrollToTop from "./components/ScrollToTop";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import ScrollToTop from "./components/layout/ScrollToTop";
 import Home from "./pages/Home";
-import Login from "./pages/Login";
-import Register from "./pages/Register";
-import ForgotPassword from "./pages/ForgotPassword";
-import VerifyEmail from "./pages/VerifyEmail";
-import ResetPassword from "./pages/ResetPassword";
+import ForgotPassword from "./pages/auth/ForgotPassword";
+import ResetPassword from "./pages/auth/ResetPassword";
+import VerifyEmail from "./pages/auth/VerifyEmail";
+import VerifyPhone from "./pages/auth/VerifyPhone";
+import Login from "./pages/auth/Login";
+import Register from "./pages/auth/Register";
+import VerifyIdentity from "./pages/auth/VerifyIdentity";
+import VerifyOtp from "./pages/auth/VerifyOTP";
+import VerifySuccess from "./pages/auth/VerifySuccess";
 import QuyDinhHoatDong from "./pages/policies/QuyDinhHoatDong";
+import ChinhSachBaoMat from "./pages/policies/ChinhSachBaoMat";
+import QuyenLoiThanhVien from "./pages/policies/QuyenLoiThanhVien";
 
 export default function App() {
   return (
-    <BrowserRouter>
+    <Router>
       <ScrollToTop />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/verify-identity" element={<VerifyIdentity />} />
+        <Route path="/verify-otp" element={<VerifyOtp />} />
         <Route path="/forgot-password" element={<ForgotPassword />} />
-        <Route path="/verify-email" element={<VerifyEmail />} />
+        <Route path="/verify-success" element={<VerifySuccess />} />
         <Route path="/reset-password" element={<ResetPassword />} />
+        <Route path="/verify-email" element={<VerifyEmail />} />
+        <Route path="/verify-phone" element={<VerifyPhone />} />
         <Route path="/quy-dinh-hoat-dong" element={<QuyDinhHoatDong />} />
+        <Route path="/chinh-sach-bao-mat" element={<ChinhSachBaoMat />} />
+        <Route path="/bang-quyen-loi-thanh-vien" element={<QuyenLoiThanhVien />} />
       </Routes>
-    </BrowserRouter>
+    </Router>
   );
 }
